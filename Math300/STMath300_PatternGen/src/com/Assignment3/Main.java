dd45b10c0f2dbcd68d26b0290402ace4ef93a7b2
Ryan Peters <petersryan84@gmail.com>
2018-04-19 05:50:22 -0700

    got math300 assig3's truth table gen up works for 1-5

---
+++ Math300/STMath300_PatternGen/src/com/Assignment3/Main.java
@@ -0,0 +1,15 @@
+package com.Assignment3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Assig3.problem1();
+        Assig3.problem2();
+        Assig3.problem4();
+        
+        Assig3.problem5();
+        System.out.println("the output for problem 5's truth tables is a bit overkill" +
+                           ", but it seemed necessary for verifying the results");
+    }
+}
