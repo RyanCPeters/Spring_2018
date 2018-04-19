fda0a0493c3a60d5e55a27c15e089c9a4ed87d24
Ryan Peters <petersRyan84@gmail.com>
2018-04-18 20:08:02 -0800

    added new files to stmath300 for generating colorized truth tables using java

---
+++ Math300/STMath300_PatternGen/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Assig3.problem1();
+        Assig3.problem2();
+    }
+}
