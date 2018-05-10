16f6f00640adb58b74f03af8dbc238751632a11f
Ryan Peters <petersRyan84@gmail.com>
2018-05-10 07:23:52 -0800

    tweaked A5P7.java to see if 3|(m*a+(n-m)*b) really is en equivalance relation

--- Math300/mathSupportCode/STMath300_PatternGen/com/Assig5/src/A5Driver.java
+++ Math300/mathSupportCode/STMath300_PatternGen/com/Assig5/src/A5Driver.java
@@ -14,7 +14,7 @@ class A5Driver
     System.out.println("Assignment 5, problem 6");
     System.out.println();
     
-    new A5P6(-20,20,-20,20);
+    new A5P6(-30,30,-30,30);
   
     System.out.println();
     System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
@@ -23,6 +23,20 @@ class A5Driver
     System.out.println();
     
     new A5P7( -20, 20, -20, 20);
+  
+    System.out.println();
+    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+    System.out.println();
+    
+    new A5P7(-20,20,-20,20,5);
+  
+    System.out.println();
+    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+    System.out.println();
+  
+    new A5P7( -20, 20, -20, 20, 5, 9);
   
     System.out.println();
     System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
--- Math300/mathSupportCode/STMath300_PatternGen/com/Assig5/src/A5P7.java
+++ Math300/mathSupportCode/STMath300_PatternGen/com/Assig5/src/A5P7.java
@@ -28,6 +28,19 @@ class A5P7
     return ((a+2*b) == 0)||(Math.abs(a + 2*b)/3 >= 1 && Math.abs( a + 2 * b )%3 ==0);
   }
   
+  private
+  boolean doesNDivide( int a, int b,int n ) {
+  
+    return ( ( a+(n-1)*b ) == 0 ) || ( Math.abs( a+(n-1)*b )/n >= 1 && Math.abs( a+(n-1)*b )%n == 0 );
+  }
+  
+  private
+  boolean doesNMDivide( int a, int b, int n,int m ) {
+    
+    return ( ( m*a+( n-m )*b ) == 0 ) || (
+      Math.abs( m*a+( n-m )*b )/n >= 1 && Math.abs( m*a+( n-m )*b )%n == 0 );
+  }
+  
   public
   A5P7() {
     this(-20,20,-20,20);
@@ -138,4 +151,228 @@ class A5P7
     System.out.println( sbPacked.toString() );
   }
   
+  A5P7( int aLo, int aHi, int bLo, int bHi, int n ) {
+    
+    int width = Math.max( String.valueOf( aLo ).length(), String.valueOf( aHi ).length() );
+    
+    width = Math.max( String.valueOf( width ).length(), String.valueOf( bLo ).length() );
+    
+    width = Math.max( String.valueOf( width ).length(), String.valueOf( bHi ).length() )+
+            ( ( bLo<0 )? 2 : 1 );
+    int packedWidth = width-( ( bLo<0 )? 2 : 1 );
+    
+    StringBuilder sbSpaced = new StringBuilder();
+    StringBuilder sbPacked = new StringBuilder();
+    
+    String
+      aAxisBounds =
+      "\nb is represented by the vertical axis, in the range of   ["+bLo+","+bHi+"]",
+      bAxisBounds =
+        "\na is represented by the horizontal axis, in the range of ["+aLo+","+aHi+"]\n\n";
+    
+    sbSpaced.append(
+      "\n"+ConsolColor.cyan_brt+"denotes where 3|(a +2b) and a < b"+ConsolColor.RESET+
+      "\n"+ConsolColor.rd_brt+"denotes where 3|(a +2b) and a > b"+ConsolColor.RESET+
+      "\n"+ConsolColor.grn_brt+"denotes where 3|(a +2b) and a == b\n\n"+ConsolColor.RESET )
+            .append( aAxisBounds ).append( bAxisBounds )
+            .append( "Figure 7.1: Stretched version of graph showing (a,b) values on the axis."+
+                     "\nWhere a is the horizontal-axis, and b is the vertical-axis.\n" );
+    
+    sbPacked.append(
+      "\n"+ConsolColor.cyan_brt+"denotes where 3|(a +2b) and a < b"+ConsolColor.RESET+
+      "\n"+ConsolColor.rd_brt+"denotes where 3|(a +2b) and a > b"+ConsolColor.RESET+
+      "\n"+ConsolColor.grn_brt+"denotes where 3|(a +2b) and a == b\n\n"+ConsolColor.RESET )
+            .append( aAxisBounds ).append( bAxisBounds )
+            .append( "Figure 7.2: Square graph for better visual understanding of ratios.\n" );
+    
+    int packedEntryWidth = 1;
+    
+    // generating a matrix of binary values that represent whether
+    // a|b or b|a.
+    //
+    //  prints 1 if true
+    //  prints 0 if false
+    // let i represent the row number in the matrix, and map to the
+    // a values on the given low to hi bounds.
+    //
+    // let j represent the column number in the matrix, and map the
+    // b values on the given low to hi bounds.
+    for( int b = bHi; b >= bLo; --b ) {
+      if( b != 0 ) sbSpaced.append( String.format( "%1s", "" ) );
+      for( int a = aLo; a<=aHi; ++a ) {
+        String locSymbol = "H";
+        
+        boolean a2b = doesNDivide( b, a,n );
+        
+        sbPacked.append( (
+                           ( b == 0 && a == 0 )? FAIL :
+                           ( a2b )?
+                           ( ( b<a )? A2B : ( b == a )? winStr : B2A ) :
+                           ( FAIL ) ) );
+        
+        sbSpaced.append( (
+                           ( b == 0 && a == 0 )? "" :
+                           ( a2b )?
+                           ( ( b<a )? A2B : ( b == a )? winStr : B2A ) :
+                           ( FAIL ) ) );
+        
+        if( b == 0 && a == 0 ) {
+          sbPacked.append( ConsolColor.RESET )
+                  .append( "0" )
+                  .append( FAIL )
+                  .append( "-" )
+                  .append( ConsolColor.RESET );
+          
+          
+          sbSpaced.append( String.format( ConsolColor.RESET+"%-"+( width )+"s", " 0" ) );
+        }
+        else
+          if( b == 0 ^ a == 0 ) {
+            String nonZeroVal = ( b != 0 )?
+                                ( String.valueOf( b ) ) :
+                                String.valueOf( a );
+            if( a == 0 && b>0 ) sbSpaced.append( String.format( "%-4s", ( " "+nonZeroVal ) ) );
+            else sbSpaced.append( String.format( "%-4s", nonZeroVal ) );
+    
+            sbSpaced.append( ConsolColor.RESET );
+    
+            sbPacked.append( ( ( b == 0 )? "--" : "| " ) )
+                    .append( ConsolColor.RESET );
+    
+          }
+          else
+            if( a2b ) {
+              sbPacked.append( locSymbol )
+                      .append( " " )
+                      .append( ConsolColor.RESET );
+              if( a == -1 ) sbSpaced.append( String.format( "%-"+( width-1 )+"s", locSymbol ) );
+              else sbSpaced.append( String.format( "%-"+( width )+"s", locSymbol ) );
+      
+              sbSpaced.append( ConsolColor.RESET );
+            }
+            else {
+              sbPacked.append( ": " );
+              if( a == -1 ) sbSpaced.append( String.format( "%-"+( width-1 )+"s", ":" ) );
+              else sbSpaced.append( String.format( "%-"+( width )+"s", ":" ) );
+            }
+      }
+      sbSpaced.append( ConsolColor.RESET ).append( "\n" );
+      sbPacked.append( ConsolColor.RESET ).append( "\n" );
+    }
+    System.out.println( sbSpaced.toString() );
+    System.out.println( sbPacked.toString() );
+  }
+  
+  
+  A5P7( int aLo, int aHi, int bLo, int bHi, int n,int m ) {
+    
+    int width = Math.max( String.valueOf( aLo ).length(), String.valueOf( aHi ).length() );
+    
+    width = Math.max( String.valueOf( width ).length(), String.valueOf( bLo ).length() );
+    
+    width = Math.max( String.valueOf( width ).length(), String.valueOf( bHi ).length() )+
+            ( ( bLo<0 )? 2 : 1 );
+    int packedWidth = width-( ( bLo<0 )? 2 : 1 );
+    
+    StringBuilder sbSpaced = new StringBuilder();
+    StringBuilder sbPacked = new StringBuilder();
+    
+    String
+      aAxisBounds =
+      "\nb is represented by the vertical axis, in the range of   ["+bLo+","+bHi+"]",
+      bAxisBounds =
+        "\na is represented by the horizontal axis, in the range of ["+aLo+","+aHi+"]\n\n";
+    
+    sbSpaced.append(
+      "\n"+ConsolColor.cyan_brt+"denotes where "+n+"|("+m+"*a +"+(n-m)+"*b) and a < b"+ConsolColor.RESET+
+      "\n"+ConsolColor.rd_brt+"denotes where "+n+"|("+m+"*a +"+(n-m)+"*b)"+ConsolColor.RESET+
+      "\n"+ConsolColor.grn_brt+"denotes where "+n+"|("+m+"*a +"+( n-m )+"*b)\n\n"+ConsolColor.RESET )
+            .append( aAxisBounds ).append( bAxisBounds )
+            .append( "Figure 7.1: Stretched version of graph showing (a,b) values on the axis."+
+                     "\nWhere a is the horizontal-axis, and b is the vertical-axis.\n" );
+    
+    sbPacked.append(
+      "\n"+ConsolColor.cyan_brt+"denotes where "+n+"|("+m+"*a +"+( n-m )+"b) and a < b"+ ConsolColor.RESET+
+      "\n"+ConsolColor.rd_brt+"denotes where "+n+"|("+m+"*a +"+( n-m )+"b)"+ConsolColor.RESET+
+      "\n"+ConsolColor.grn_brt+"denotes where "+n+"|("+m+"*a +"+( n-m )+"b)\n\n"+ConsolColor.RESET )
+            .append( aAxisBounds ).append( bAxisBounds )
+            .append( "Figure 7.2: Square graph for better visual understanding of ratios.\n" );
+    
+    int packedEntryWidth = 1;
+    
+    // generating a matrix of binary values that represent whether
+    // a|b or b|a.
+    //
+    //  prints 1 if true
+    //  prints 0 if false
+    // let i represent the row number in the matrix, and map to the
+    // a values on the given low to hi bounds.
+    //
+    // let j represent the column number in the matrix, and map the
+    // b values on the given low to hi bounds.
+    for( int b = bHi; b >= bLo; --b ) {
+      if( b != 0 ) sbSpaced.append( String.format( "%1s", "" ) );
+      for( int a = aLo; a<=aHi; ++a ) {
+        String locSymbol = "H";
+        
+        boolean a2b = doesNMDivide( b, a, n,m );
+        
+        sbPacked.append( (
+                           ( b == 0 && a == 0 )? FAIL :
+                           ( a2b )?
+                           ( ( b<a )? A2B : ( b == a )? winStr : B2A ) :
+                           ( FAIL ) ) );
+        
+        sbSpaced.append( (
+                           ( b == 0 && a == 0 )? "" :
+                           ( a2b )?
+                           ( ( b<a )? A2B : ( b == a )? winStr : B2A ) :
+                           ( FAIL ) ) );
+        
+        if( b == 0 && a == 0 ) {
+          sbPacked.append( ConsolColor.RESET )
+                  .append( "0" )
+                  .append( FAIL )
+                  .append( "-" )
+                  .append( ConsolColor.RESET );
+          
+          
+          sbSpaced.append( String.format( ConsolColor.RESET+"%-"+( width )+"s", " 0" ) );
+        }
+        else
+          if( b == 0 ^ a == 0 ) {
+            String nonZeroVal = ( b != 0 )?
+                                ( String.valueOf( b ) ) :
+                                String.valueOf( a );
+            if( a == 0 && b>0 ) sbSpaced.append( String.format( "%-4s", ( " "+nonZeroVal ) ) );
+            else sbSpaced.append( String.format( "%-4s", nonZeroVal ) );
+            
+            sbSpaced.append( ConsolColor.RESET );
+            
+            sbPacked.append( ( ( b == 0 )? "--" : "| " ) )
+                    .append( ConsolColor.RESET );
+            
+          }
+          else
+            if( a2b ) {
+              sbPacked.append( locSymbol )
+                      .append( " " )
+                      .append( ConsolColor.RESET );
+              if( a == -1 ) sbSpaced.append( String.format( "%-"+( width-1 )+"s", locSymbol ) );
+              else sbSpaced.append( String.format( "%-"+( width )+"s", locSymbol ) );
+              
+              sbSpaced.append( ConsolColor.RESET );
+            }
+            else {
+              sbPacked.append( ": " );
+              if( a == -1 ) sbSpaced.append( String.format( "%-"+( width-1 )+"s", ":" ) );
+              else sbSpaced.append( String.format( "%-"+( width )+"s", ":" ) );
+            }
+      }
+      sbSpaced.append( ConsolColor.RESET ).append( "\n" );
+      sbPacked.append( ConsolColor.RESET ).append( "\n" );
+    }
+    System.out.println( sbSpaced.toString() );
+    System.out.println( sbPacked.toString() );
+  }
 }
