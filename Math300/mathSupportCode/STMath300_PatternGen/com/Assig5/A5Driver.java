05ce322cab241882e4d6f8deb763b07ce5f7b3ea
Ryan Peters <petersryan84@gmail.com>
2018-07-11 16:03:37 -0700

    updated math 300's assig5 driver

--- Math300/mathSupportCode/STMath300_PatternGen/com/Assig5/A5Driver.java
+++ Math300/mathSupportCode/STMath300_PatternGen/com/Assig5/A5Driver.java
@@ -1,4 +1,4 @@
-package Assig5;
+package com.Assig5;
 import Assig5.problemFiles.A5P5;
 import Assig5.problemFiles.A5P6;
 import Assig5.problemFiles.A5P7;
@@ -20,33 +20,33 @@ class A5Driver
     System.out.println();
     new A5P5();
 //    new A5P5(-20,20,-20,20,new int[]{-20,20});
-    System.out.println();
-    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
-    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
-    System.out.println( "Assignment 5, problem 6");
-    System.out.println();
-
-    new A5P6( -20, 20, -20, 20);
-
-    System.out.println();
-    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
-    System.out.println( String.format( "%90s","" ).replace( " ", "-" ));
-    System.out.println( "Assignment 5, problem 7");
-    System.out.println();
-
-    new A5P7( -20, 20, -20, 20);
-
-  
-    System.out.println();
-    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
-    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
-    System.out.println();
-
-    new A5P7( -20, 20, -20, 20, 17, 11);
-
-    System.out.println();
-    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
-    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
-    System.out.println();
+//    System.out.println();
+//    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+//    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+//    System.out.println( "Assignment 5, problem 6");
+//    System.out.println();
+//
+//    new A5P6( -20, 20, -20, 20);
+//
+//    System.out.println();
+//    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+//    System.out.println( String.format( "%90s","" ).replace( " ", "-" ));
+//    System.out.println( "Assignment 5, problem 7");
+//    System.out.println();
+//
+//    new A5P7( -20, 20, -20, 20);
+//
+//
+//    System.out.println();
+//    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+//    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+//    System.out.println();
+//
+//    new A5P7( -20, 20, -20, 20, 17, 11);
+//
+//    System.out.println();
+//    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+//    System.out.println( String.format( "%90s", "" ).replace( " ", "-" ) );
+//    System.out.println();
   }
 }
